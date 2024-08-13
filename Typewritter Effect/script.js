58081fc3ad3fc48e3d9449f1bd805e602e7e98d9
Christiaan-C-Cupido <3654647@myuwc.ac.za>
2024-08-13 16:02:22 +0200

    update

--- Typewritter Effect/script.js
+++ Typewritter Effect/script.js
@@ -16,8 +16,30 @@ words.forEach(word => {
 
     textTimeline.to('#typewriter', {
         text: word,
-        duration: 1
+        duration: 1,
+        onUpdate: () => {
+            cursorTimeline.restart()
+            cursorTimeline.pause()
+        },
+        onComplete: () => {
+            cursorTimeline.play()
+        }
     })
 
     mainTimeline.add(textTimeline)
+})
+
+//Blinking cursor
+let cursorTimeline = gsap.timeline({
+    repeat: -1,
+    repeatDelay: .8
+})
+
+cursorTimeline.to('#cursor', {
+    opacity: 1,
+    duration: 0
+}).to('#cursor', {
+    opacity: 0,
+    duration: 0,
+    delay: .8
 })
